8f90fb42ec30fc536eb246387c4f034879ef82fd
Nate Sampo <nathan.sampo@students.olin.edu>
2020-02-16 21:40:01 -0500

    fixed for back compatibility

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var io = socketIO(server);
 
 var port = 5000;
 var ticks = 2;
-var time = Date.getTime();
+var date = new Date();
+var time = date.getTime();
 
 var species = [];
 var client_ids = [];
@@ -145,7 +146,8 @@ function packet() {
 }
 
 setInterval(function() {
-	var newTime = Date.getTime();
+	var date = new Date();
+	var newTime = date.getTime();
 	var dt = time - newTime;
 
 	for (var i in client_ids) {
